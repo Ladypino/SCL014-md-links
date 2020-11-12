b902c7227ce4b63e46d8020c7d5fa5f727368fcb
lady pino <ladypino112@gmail.com>
2020-11-12 07:03:29 -0300

    add file read folders add test branch

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
+/* console.log('Es el mapa es el mapa es el mapa  es el mapa ES EL MAPAAAA!!!!!');
+console.log(process.argv[2]);
+
+process.argv
+ */
+
+
+/* module.exports = () => {
   // ...
-};
+};node index.js
+ */
+
+ console.log('hola',process.argv[2],'!!!')
